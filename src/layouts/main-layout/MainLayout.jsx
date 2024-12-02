6a6d06f57b25654d98b1064b455f964756f83086
nastyazz <nastayz0808@mail.ru>
2024-12-02 22:40:12 +0300

    feat: Добавлен footer на страницах

--- src/layouts/main-layout/MainLayout.jsx
+++ src/layouts/main-layout/MainLayout.jsx
@@ -4,13 +4,19 @@ import Header from "../../components/header/header";
 
 const MainLayout = () => {
     return (
-        <div>
-            <Header></Header>
-            <main style={{ paddingTop: '80px' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+            <Header />
+            <main style={{ paddingTop: '60px', flex: 1 }}>
                 <Outlet />
             </main>
+            <footer className="footer">
+                <div className="footer-content">
+                    <div className="footer-line"></div>
+                    <span className="footer-text">2024 Моя компания</span>
+                </div>
+            </footer>
         </div>
-    )
-}
+    );
+};
 
 export default MainLayout;
